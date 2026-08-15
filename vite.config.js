9fbac55671c5d93b2c89de29ac59c5b9f2d50c92
Hima P H <himaph05@gmail.com>
2026-08-15 18:44:47 +0000

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig, transformWithEsbuild } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    {
-      name: "jsx-in-js",
-      enforce: "pre",
-
-      async transform(code, id) {
-        if (id.includes("/src/") && id.endsWith(".js")) {
-          return transformWithEsbuild(code, id, {
-            loader: "jsx",
-            jsx: "automatic",
-          });
-        }
-
-        return null;
-      },
-    },
-
-    react(),
-  ],
+  base: "/Like-Card/",
+  plugins: [react()],
 });
